741c3b3bfb37f15c8d19d44b025bbb95895344f1
Jeferson Dias <jefersondias6711@gmail.com>
2021-11-01 08:38:53 -0300

    Mudando layout de conteúdo

--- pages/[section]/[page].js
+++ pages/[section]/[page].js
@@ -3,20 +3,30 @@ import axios from "axios"
 import Image from 'next/image'
 
 export default function Page({title,cont}){
+  console.log(cont)
   return(
       <MyModal title={title}>
         {
-          cont.map(resp=>{
-          if(resp.type=="paragraph"){
-            return(resp[resp.type].text[0]?.href ? <a key={resp.id} href={resp[resp.type].text[0]?.href}>{resp[resp.type].text[0]?.plain_text}</a>:<p key={resp.id}>{resp[resp.type].text[0]?.plain_text}</p>)
-          }else if (resp.type=="bulleted_list_item"){
-            return(<ul key={resp.id}><li>{resp[resp.type].text[0]?.plain_text}</li></ul>)
-          }else if (resp.type=="image"){
-            return(<Image alt={'img '+resp.id} key={resp.id} src={resp[resp.type].external.url}/>)
-          }else if(resp.type=="bookmark"){
-            return (<iframe key={resp.id} src={`${resp.bookmark.url}`} width='100%' height='100%'></iframe>)
-          }
-      })}
+          cont.map((resp,index)=>{
+            switch(resp.type){
+              case 'paragraph':
+                return(resp[resp.type].text.map(x=>{ return( x.href ? <a key={resp.id} href={x[0]?.href}><p>{x?.plain_text}</p></a>:<p key={resp.id}>{x?.plain_text}</p>)})) 
+                break;
+              case "bulleted_list_item" :
+                return(<ul key={resp.id}><li>{resp[resp.type].text[index]?.plain_text}</li></ul>)
+                break;
+              case "image" :
+                return(<Image alt={'img '+resp.id} key={resp.id} src={resp[resp.type].external.url}/>)
+                break;
+              case "bookmark":
+                return(<iframe key={resp.id} src={`${resp.bookmark.url}`} width='100%' height='100%'></iframe>)
+                break;
+              default :
+                return(<div></div>)
+                break;
+            }
+          })
+      }
       </MyModal>  
   )
 }
@@ -40,11 +50,6 @@ export async function getStaticPaths(){
       }
     }
   })
-
-  
-
-  
-
   return{
     paths,
     fallback:false
@@ -66,19 +71,40 @@ export async function getStaticProps(context){
   const respGet = await axios('https://api.notion.com/v1/blocks/'+block_id+'/children',paramsGet)
   const dataGet = await respGet.data.results
 
-  const resp = await axios('https://api.notion.com/v1/databases/ee7c4808765e4e438e09979102edb518/',paramsGet)
-  const data = await resp.data
-
-  const paramsPost = {
-    headers:{
-        "Authorization": process.env.NOTION_KEY,
-        "Notion-Version": "2021-08-16"
-    },
-    method:'post'
+  function getChild(data){
+    data.forEach(async (x,index)=>{
+      if(x.has_children){
+        const respChild = await axios('https://api.notion.com/v1/blocks/'+x.id+'/children',paramsGet)
+        x.child = await respChild.data.results
+        getChild(x.child)
+      }
+    })
   }
-  const respPost = await axios('https://api.notion.com/v1/databases/ee7c4808765e4e438e09979102edb518/query',paramsPost)
-  const dataPost = await respPost.data.results
+  getChild(dataGet)
 
+  /*if(dataGet.has_children){
+    const respChild = await axios('https://api.notion.com/v1/blocks/'+dataGet.id+'/children',paramsGet)
+    dataGet.last_edited_time = await respChild.data
+
+  }
+
+
+
+
+  /*   Get para o menu  */
+    const resp = await axios('https://api.notion.com/v1/databases/ee7c4808765e4e438e09979102edb518/',paramsGet)
+    const data = await resp.data
+
+    const paramsPost = {
+      headers:{
+          "Authorization": process.env.NOTION_KEY,
+          "Notion-Version": "2021-08-16"
+      },
+      method:'post'
+    }
+    const respPost = await axios('https://api.notion.com/v1/databases/ee7c4808765e4e438e09979102edb518/query',paramsPost)
+    const dataPost = await respPost.data.results
+  /*   Get para o menu  */
   return {
       props:{
           cont:dataGet,
